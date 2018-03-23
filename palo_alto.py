ba8b138baa4ba17d8abd623ddf3e9fecfbd901cd
Adam Hicks <thomas.adam.hicks@gmail.com>
2018-03-22 20:36:24 -0600

    a new take

--- palo_alto.py
+++ palo_alto.py
@@ -7,13 +7,47 @@ import json
 import ssl
 
 
-def palo(pa_ip, pa_key, cmd, parameters):
+def palo(pa_ip, pa_key, cmd, RULENAME=None, IP_ADDR=None, call_type):
 	
 	ctx = ssl.create_default_context()
 	ctx.check_hostname = False
 	ctx.verify_mode = ssl.CERT_NONE
 	
-	parameters = json.loads(parameters)
+	if call_type == "A":
+
+		parameters = {
+			"xpath": "/config/devices/entry/vsys/entry[@name=\'vsys4\'']/rulebase/security/rules/entry[@name=%s]" % (RULENAME), 
+			"element": """
+			<source><member:Inbound-Global-F5</member></source>
+			<destination><member>%s</member></destination>
+			<service><member>application-default</member></service>
+			<application>
+			<member>ssl</member>
+			<member>ping</member>
+			</application>
+			<category><member>any</member></category>
+			<hip-profiles><member>any</member></hip-profiles><action>allow</action><source-user><member>any</member></source-user>
+			<log-start>yes</log-start>
+			<log-end>no</log-end>
+			<description>morpheus testing</description>
+			<from><member>infra-untrust</member></from>
+			<to><member>infra-public</member></to>
+			""" % (IP_ADDR)
+		}
+	elif call_type == "B":
+
+		parameters = {
+			"xpath": "/config/devices/entry/vsys/entry[@name=\'vsys4\']/rulebase/security/rules/entry[@name=%s]" % (RULENAME),
+			"where": "before", 
+			"dst": "Deny All"
+		}
+	elif call_type == "C":
+		parameters = {
+			"cmd": "<commit></commit>"
+		}
+	else:
+		print "INSUFFICIENT CALL TYPE"
+		sys.exit(1)
 	
 	url = "https://"+pa_ip+cmd+"Key="+pa_key+"&"+urllib.urlencode(parameters)
 	
@@ -32,6 +66,8 @@ if __name__ == "__main__":
 	pa_ip = sys.argv[1]
 	pa_key = sys.argv[2]
 	cmd = sys.argv[3]
-	parameters = sys.argv[4]
+	RULENAME = sys.argv[4]
+	IP_ADDR = sys.argv[5]
+	call_type = sys.argv[6]
 	
-	palo(pa_ip, pa_key, cmd, parameters)
+	palo(pa_ip, pa_key, cmd, RULENAME, IP_ADDR, call_type)
